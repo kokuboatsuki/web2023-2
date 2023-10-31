e89a72d1c0a6f2c138755ad27308a30156291e0d
kokuboatsuki <131725519+kokuboatsuki@users.noreply.github.com>
2023-10-31 10:22:01 +0900

    Create ex6-6.js

---
+++ ex6-6.js
@@ -0,0 +1,18 @@
+var who66 = 'world';
+function sayhello66() {
+    alert('Hello,' + who66 + '!');
+}
+
+function taro66() {
+    who66 = '太郎';
+    taro666();
+}
+
+function hanako66() {
+    who66 = '花子';
+    taro666();   
+}
+
+function taro666() {
+    alert(who66 + 'が来た');
+}
