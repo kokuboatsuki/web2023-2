8147ff22f5808fc08c25d1b42442512e64d59d06
kokuboatsuki <131725519+kokuboatsuki@users.noreply.github.com>
2023-12-01 00:47:53 +0900

    Update ex7-11.js

--- ex7-11.js
+++ ex7-11.js
@@ -1,3 +1,14 @@
 function uru01() {
-    var u = document.getElementById('uru1').value;
+    var year = document.getElementById('uru1').value;
+
+    if (!year) {
+        alert("数字が入力されていません");
+        return;
+    }
+
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+        alert("うるう年です");
+    } else {
+        alert("うるう年ではありません");
+    }
 }
