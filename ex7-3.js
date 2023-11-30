93cd869193f20dd5bd3238fb0b033f383d57481d
kokuboatsuki <131725519+kokuboatsuki@users.noreply.github.com>
2023-12-01 01:16:45 +0900

    Update ex7-3.js

--- ex7-3.js
+++ ex7-3.js
@@ -1,4 +1,4 @@
-function add(){
+function add3(){
     var x = document.getElementById('bmi1').value;
     var y = document.getElementById('bmi2').value;
     alert(Math.round(Number(y) / (Number((x / 100)**2))*10)/10);
