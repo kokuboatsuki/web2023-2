70039e0cbed4bdf2c45053ef08d79bea18841362
kokuboatsuki <131725519+kokuboatsuki@users.noreply.github.com>
2023-12-01 00:08:40 +0900

    Create ex7-9.js

---
+++ ex7-9.js
@@ -0,0 +1,11 @@
+function nazo9(){
+    if (document.getElementById('answer9').value == '夢'){
+        alert('あたり！');
+    } 
+    else if (document.getElementById('answer9').value == 'ゆめ'){
+        alert('あたり！')
+    }
+    else {
+        alert('はずれ！');
+    }
+}
